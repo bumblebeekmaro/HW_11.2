23e3be6c9724998ba5dd29b0a8887e2c14233ba8
bumblebeekmaro <bumblebeekmaro@yandex.ru>
2024-03-02 16:02:31 +0300

    .capitalize_string docstrings added capitalize_words function added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,24 @@
 def capitalize_string(input_string):
+    """
+        Функция преобразует входную строку в верхний регистр.
+
+        Аргументы:
+            input_string (str): Входная строка.
+
+        Возвращает:
+            str: Строка, в которой все символы приведены к верхнему регистру.
+        """
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """
+    Функция для заглавных букв каждого слова в строке.
+
+    Аргументы:
+        input_string (str): Входная строка.
+
+    Возвращает:
+        str: Строка с заглавными буквами первой буквы каждого слова.
+    """
+    return ' '.join(word.capitalize() for word in input_string.split())
